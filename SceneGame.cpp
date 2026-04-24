9b868b507018558dbbd9d6a1c9a50567ab073d5d
OnigiriMaru1939 <game.dev.acco@gmail.com>
2026-04-24 12:22:31 +0900

    Playerを追加

--- SceneGame.cpp
+++ SceneGame.cpp
@@ -1,9 +1,11 @@
 ﻿#include "SceneGame.h"
 #include "InputManager.h"
+#include "Player.h"
 
 SceneGame::SceneGame(FileManager& fileMng) : SceneSuper(fileMng)
 {
-
+	player = std::make_unique<Player>();
+	player->SystemInit();
 }
 
 SceneGame::~SceneGame()
@@ -13,6 +15,8 @@ SceneGame::~SceneGame()
 
 void SceneGame::Update()
 {
+	player->Update();
+
 	if (InputManager::GetInstance().IsKeyTriggered(KEY_INPUT_RETURN))
 	{
 		SetNextScene(SceneID::RESULT);
@@ -24,4 +28,5 @@ void SceneGame::Draw()
 {
 	DrawBox(0, 0, Application::SCREEN_WID, Application::SCREEN_HIG, 0x00aa00, true);
 	DrawString(0, 20, "GAME SCENE", 0xffffff);
+	player->Draw();
 }
