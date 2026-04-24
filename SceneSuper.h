461ca4f3fe6f9200f1e1c8507b46282fcb703b31
K-Snail365 <K.Snail.sus4@gmail.com>
2026-04-24 09:40:37 +0900

    [変更] 使わない関数や変数をコメントアウト

--- SceneSuper.h
+++ SceneSuper.h
@@ -18,22 +18,21 @@ public:
 	// SceneからFileManagerを消さないようにポインタ参照
 	SceneSuper(FileManager& fileMng) : fileMng_(fileMng) {};
 	virtual ~SceneSuper() = default;
-	// 戻り値: true = シーン継続, false = シーン終了
 	virtual void Update() = 0;
 	virtual void Draw() = 0;
 
 	bool IsEnd() const { return isEnd; }
 	SceneID GetNextScene() const { return nextSceneID; }
 	void SetNextScene(SceneID next) { nextSceneID = next; }
-	float EaseInOut(float t) { return t * t * (3.0f - 2.0f * t); }
+	//float EaseInOut(float t) { return t * t * (3.0f - 2.0f * t); }
 
-	virtual void SetTransitionIn(float t) {}
-	virtual void SetTransitionOut(float t) {}
-	void SetIsTransition(bool b) { isTransition = b; }
-	bool GetIsTransition() { return isTransition; }
+	//virtual void SetTransitionIn(float t) {}
+	//virtual void SetTransitionOut(float t) {}
+	//void SetIsTransition(bool b) { isTransition = b; }
+	//bool GetIsTransition() { return isTransition; }
 protected:
 	FileManager& fileMng_;
 	bool isEnd = false;
-	bool isTransition = false;
+	//bool isTransition = false;
 	SceneID nextSceneID = SceneID::NONE;
 };
