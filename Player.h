2a2a004b0d12a6354f47dac69ea786548cab37fe
OnigiriMaru1939 <game.dev.acco@gmail.com>
2026-05-11 13:56:49 +0900

    プレイヤーのHP処理・炭酸残量ゲージの非表示

--- Player.h
+++ Player.h
@@ -12,22 +12,22 @@ class Player
 public:
 	static constexpr int PLAYER_WIDTH = 64;			//プレイヤーの横幅
 	static constexpr int PLAYER_HEIGHT = 64;		//プレイヤーの縦幅
-	static constexpr int SPEED = 15;					//プレイヤーの移動速度
-	static constexpr int PLAYER_SUM = 3;			//プレイヤーの残機数
+	static constexpr int SPEED = 15;				//プレイヤーの移動速度
 
 	Player(FileManager& fileMng);
 	~Player();
-	bool SystemInit();			//初期化
+	bool SystemInit();								//初期化
 	bool SetImage(const std::string& path);			//画像のセット
 	void Update();
 	void Draw();
-	void SodaShake();			//マウスを振ると炭酸蓄積ゲージが溜まる
-	void SodaGaugeCharge();		//炭酸残量ゲージの自然回復
-	void AddGravity();			//重力処理
-	void SodaMove();			//炭酸移動処理
-	void SpaceJump();			//スペースジャンプ処理
-	void Rotate();				//回転処理
-	void SodaAttack();			//炭酸攻撃処理
+	void SodaShake();				//マウスを振ると炭酸蓄積ゲージが溜まる
+	void SodaGaugeCharge();			//炭酸残量ゲージの自然回復
+	void AddGravity();				//重力処理
+	void SodaMove();				//炭酸移動処理
+	void SpaceJump();				//スペースジャンプ処理
+	void Rotate();					//回転処理
+	void SodaAttack();				//炭酸攻撃処理
+	void Damage(float damage);		//ダメージ処理
 
 	int prevMouseX;				//前回のマウスX座標
 	int prevMouseY;				//前回のマウスY座標
@@ -35,20 +35,27 @@ public:
 	float posX;					//プレイヤーのX座標
 	float posY;					//プレイヤーのY座標
 
-	int GetWidth() const;		//プレイヤーの画像の幅を取得
-	int GetHeight() const;		//プレイヤーの画像の高さを取得
+	float playerHp;				//プレイヤーのHP
+	float playerHpMax;			//プレイヤーのHPの最大値
+	float sodaGauge;			//炭酸残量ゲージ
+	float sodaGaugeMax;			//炭酸残量ゲージの最大値
+	float sodaShakeGauge;		//炭酸蓄積ゲージ
+	float sodaShakeGaugeMax;	//炭酸蓄積ゲージの最大値
 
 private:
+	int GetWidth() const;									//プレイヤーの画像の幅を取得
+	int GetHeight() const;									//プレイヤーの画像の高さを取得
+	bool GetAttakFlag() const { return sodaAttackFlag; }	//炭酸攻撃フラグを取得
+	bool GetAliveFlag() const { return aliveFlag; }			//生存フラグを取得
+	bool GetJumpFlag() const { return jumpFlag; }			//ジャンプフラグを取得
+
 	FileManager& fileManager;			//ファイルマネージャー
 	std::shared_ptr<ImageFile> image_;	//プレイヤーの画像
 
 	int width_;					//プレイヤーの画像の幅
 	int height_;				//プレイヤーの画像の高さ
 
-	float sodaGauge;			//炭酸残量ゲージ
-	float sodaGaugeMax;			//炭酸残量ゲージの最大値
-	float sodaShakeGauge;		//炭酸蓄積ゲージ
-	float sodaShakeGaugeMax;	//炭酸蓄積ゲージの最大値
+	
 	float sodaPower;			//炭酸攻撃の威力
 	bool aliveFlag;				//生存フラグ
 	bool jumpFlag;				//ジャンプフラグ
