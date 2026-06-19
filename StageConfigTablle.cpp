f75510b9ea9aba18fb7cc6f7254cd5462f9f3673
AzureAD\甲斐拓未 <kasiwagohan1@gmail.com>
2026-06-19 15:12:47 +0900

    ステージ２改良

--- StageConfigTablle.cpp
+++ StageConfigTablle.cpp
@@ -39,6 +39,8 @@ const std::vector<StageConfig>& GetStageConfigs()
 			scene.GetStage()->SetMchipImage("Resource/MapChip/Mapchip_neon.png");
 			//プレイヤーの初期化
 			 scene.GetPlayer()->SetPosition(100.0f, 3450.0f);
+			//ボスエリアの初期設定
+			scene.SetBossArea(80, 4600, 1900, 6500);
 			//敵の生成
 			scene.AddEnemy(EnemyBase::ENEMY_TYPE::E_TYPE_1, 500.0f, 200.0f);
 			scene.AddEnemy(EnemyBase::ENEMY_TYPE::E_TYPE_1, 800.0f, 1700.0f);
